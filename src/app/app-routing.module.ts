0a4a2c541c157616a4d293ffb92440986b8b0354
Adrian <adrianp1447@gmail.com>
2022-10-04 16:21:21 -0400

    created first routes and home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateNewTestComponent } from './components/create-new-test/create-new-test.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'createNewTest', component: CreateNewTestComponent }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
